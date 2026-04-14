9e590a30855e19293b65fcc6eb2b19e9e90d276a
Pratham Prajapati <prathamprajapati4071@gmail.com>
2026-04-14 15:24:22 +0530

    Updated URL

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
     // Replace this with your Render backend URL when deployed
-    API_BASE_URL: 'https://proactivecareequipment.onrender.com'
+    API_BASE_URL: 'https://proactivecareequipment-1.onrender.com'
     // API_BASE_URL: 'http://localhost:5000'
 };
